354728357085977d9952d1dec3889e4861bcf5e8
yzou235 <yumingzou235@gmail.com>
2023-12-29 21:55:46 -0500

    Finished II. A B & C

--- script.js
+++ script.js
@@ -61,3 +61,45 @@ console.log(animal === "cow" ? "mooooo" : "Hey! You are not a cow.");
 let age = 60;
 console.log(age >= 16 ? "Here are the keys!" : "Sorry, you're too young.");
 
+
+///////////////
+// II. Loops //
+///////////////
+
+// A The basics //
+
+// 1. Write a loop that will print out all the numbers from 0 to 10, inclusive
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+// 2. Write a loop that will print out all the numbers from 10 up to and including 400
+for (let i = 10; i <= 400; i++) {
+    console.log(i);
+}
+// 3. Write a loop that will print out every third number starting with 12 and going no higher than 4000
+for (let i = 12; i <= 4000; i += 3) {
+    console.log(i);
+}
+
+// B. Get even //
+
+// 1. Print out the numbers that are within the range of 1 - 100
+// 2. Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is an even number");
+    } else {
+        console.log (i);
+    }
+}
+
+// C. Give me Five //
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`I found a ${i}. High five! Three is a crowd`);
+    } else if (i % 5 === 0) {
+        console.log(`I found a ${i}. High five!`);
+    } else if (i % 3 === 0) {
+        console.log(`I found a ${i}. Three is a crowd`);
+    }
+}
