7f323ef4d67bc7eb90bb853e2cf9cad4bf3b65ac
yzou235 <yumingzou235@gmail.com>
2023-12-29 21:33:14 -0500

    Finished I.A

---
+++ script.js
@@ -0,0 +1,25 @@
+//////////////////////////////
+// I. Variables & Datatypes //
+//////////////////////////////
+
+// A. Q + A //
+
+/*
+1. How do we assign a value to a variable?
+- Use "=" assignment operator.
+2. How do we change the value of a variable?
+- Reassign it using the assignment operator.
+3. How do we assign an existing variable to a new variable?
+- newVar = existingVar
+4. Remind me, what are declare, assign, and define?
+- Declare: The process of introducing a variable to the compiler or interpreter. It tells the compiler that the variable exists and specifies its data type but doesn't allocate memory or assign a value.
+- Assign: The process of giving a value to a variable. It involves using the assignment operator to store a particular value in a variable.
+- Define: the definition specifies the body for a function, associates an identifier with the function, and allocates storage for it.
+5. What is pseudocoding and why should you do it?
+- Pseudocoding is the process of planning a computer program by writing a high-level description of its logic, structure, and flow using natural language rather than a specific programming language syntax. It helps in organizing thoughts before actual coding begins and serves as a roadmap for implementation. 
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+-  A common guideline is to spend a significant amount of time upfront on problem-solving, design, and planning (possibly 50-80%), and the remaining time on actual implementation. 
+ */
+
+// B. Strings
+
